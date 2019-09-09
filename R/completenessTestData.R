567f976ec799d26484b891e7a4d1037e21572a64
Yang Xie <yang@bi-69.gobics.de>
2019-09-09 12:53:31 +0200

    neu

--- R/completenessTestData.R
+++ R/completenessTestData.R
@@ -2,56 +2,68 @@
 #' @importFrom Rcpp sourceCpp
 #' @export
 completenessTestData <- function(data,catalogue,minContigLength,meanContigLength,number,seed = 0,distr = "normal",comp = c(0.6,1.0),cont = c(0.0,0.4)){
-    x = Sys.time()
+    y = Sys.time()
     data = data[unique(names(data))]
     cat = subset(catalogue,GI.Vec %in% names(data))
     nms = names(data) %in% cat$GI.Vec
     IDs = names(data)[nms]
     tmp1 = list()
     isUsed = c()
+   
+    n = 1
     for(i in 1:length(cat$comb)){
         if(!(cat$comb[i] %in% isUsed)){
-            tmp1[[length(tmp1)+1]] = cat$GI.Vec[which(cat$comb == cat$comb[i])]
-            isUsed[length(isUsed)+1] = cat$comb[i]
-            
+            tmp1[[n]] = cat$GI.Vec[which(cat$comb == cat$comb[i])]
+            isUsed[n] = cat$comb[i]
+            n = n+1
         }
     }
+    
     lengths = list()
     lengthSums = c()
     accession = list()
+    n = 1
     for(i in 1:length(tmp1)){
         tmp2 = c()
         tmp5 = c()
         lengthSums[i] = 0
+        n = 1
         for(j in 1:length(tmp1[[i]])){
             tmp5[j] = which(IDs == tmp1[[i]][j]) -1
-            tmp2[length(tmp2)+1] = as.numeric(tmp1[[i]][j])
-            tmp2[length(tmp2)+1] = sum(data[[tmp1[[i]][j]]]$GENOME[[1]]@lengths)
+            tmp2[n] = as.numeric(tmp1[[i]][j])
+            tmp2[n+1] = sum(data[[tmp1[[i]][j]]]$GENOME[[1]]@lengths)
             lengthSums[i] = lengthSums[i] + tmp2[length(tmp2)]
+            n = n +2
         }
         lengths[[i]] = tmp2
         accession[[i]] = tmp5
     } 
+    
     pifams = list()
     Orfs = list()
+    m = 1
     for(i in 1:length(data)){
         if(nms[i]){
             tmp3 = list()
             tmp4 = list()
+            n = 1
             for(j in 1:length(data[[i]]$GENOME)){
                 if((!is.null(data[[i]]$GENOME[[j]])) && (!is.null(data[[i]]$ORF[[j]]))){
-                    tmp3[[length(tmp3)+1]] = data[[i]]$GENOME[[j]]@lengths
-                    tmp3[[length(tmp3)+1]] = data[[i]]$GENOME[[j]]@values
-                    tmp4[[length(tmp4)+1]] = data[[i]]$ORF[[j]]@lengths
-                    tmp4[[length(tmp4)+1]] = data[[i]]$ORF[[j]]@values
+                    tmp3[[n]] = data[[i]]$GENOME[[j]]@lengths
+                    tmp3[[n+1]] = data[[i]]$GENOME[[j]]@values
+                    tmp4[[n]] = data[[i]]$ORF[[j]]@lengths
+                    tmp4[[n+1]] = data[[i]]$ORF[[j]]@values
+                    n = n +2
                 }
             }
-            pifams[[length(pifams)+1]] = tmp3
-            Orfs[[length(Orfs)+1]] = tmp4
+            pifams[[m]] = tmp3
+            Orfs[[m]] = tmp4
+            m = m +1
         }
     }
+    
     if(length(lengths) > 1){
-        print(Sys.time() -x)
+        print(Sys.time() -y)
         x = Sys.time()
         res = compTestData(pifams,Orfs,lengths,lengthSums,minContigLength,meanContigLength,number,comp,cont,accession,seed,distr)
     }
@@ -59,11 +71,11 @@ completenessTestData <- function(data,catalogue,minContigLength,meanContigLength
         res = list()
         print("zu wenig Genome in dem Daten Satz")
     }
+    
     print(Sys.time() -x)
+    print(Sys.time() -y)
     return(res)
 }
-
-
 
 
 timeCompletenessTestData <- function(times,data,catalogue,minContigLength,meanContigLength,seed = 0){
