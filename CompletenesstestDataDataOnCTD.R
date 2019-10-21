6f06991d467f5bf2136112d7721c817a6a4b6472
YangXie93 <38864295+YangXie93@users.noreply.github.com>
2019-10-21 14:32:26 +0200

    Delete CompletenesstestDataDataOnCTD.R

--- CompletenesstestDataDataOnCTD.R
+++
@@ -1,9 +0,0 @@
-Daten zur Differenz von SummebaseCount/gesamteBasenZahlderPifams und completeness|contamination, sowie Daten zur Differenz von SummepifamCount/gesamteAnzahlAllerPifams und completeness|contamination
-
-100 X 1000 beispiele:
-[1] "mean baseCount diff:  0.0119906424412745"
-[1] "sd baseCount diff:  0.0132416884278999"
-[1] "max base diff:  0.0861044651258722 min base diff:  -0.0363461754937908"
-[1] "meen pifamCount diff:  -0.0469826511225725"
-[1] "sd pifamCount diff:  0.0320639769006482"
-[1] "max pifam diff:  0.0189575960932172 min pifam diff:  -0.118350322245303"
