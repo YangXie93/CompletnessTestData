4835e126c22fef0ad7c51f8e7e4086e4b6948dfa
Yang Xie <yang@bi-69.gobics.de>
2019-10-21 14:52:57 +0200

    folder deleted

--- testScripts/buggy.R
+++
@@ -1,9 +0,0 @@
-rm(list = ls())
-library(CompletenessTestData)
-library(IRanges)
-data = readRDS("~/Daten/data_red.Rds")
-cat = readRDS("~/Daten/bac_dt.Rds")$DD
-
-
-x = completenessTestData(data,cat,10000,15000,10000,seed =1)
-print(x[[44]])
